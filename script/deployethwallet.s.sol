95a3703abea2c5824d61b3e35536a83b6da2d427
root <root@DESKTOP-PI8J1VA>
2024-03-14 14:34:36 +0530

    Deploy script update

--- script/deployethwallet.s.sol
+++ script/deployethwallet.s.sol
@@ -4,4 +4,11 @@ pragma solidity ^0.8.24;
 import {Script} from "forge-std/Script.sol";
 import {ethwallet} from "../src/ethwallet.sol";
 
-contract deploy is Script {}
+contract deploy is Script {
+    function run() external returns (ethwallet) {
+        vm.startBroadcast();
+        ethwallet Ethwallet = new ethwallet();
+        vm.stopBroadcast();
+        return Ethwallet;
+    }
+}
